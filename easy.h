acf76a562e2ea34b5eb4f67f85e2d5a5ef472346
TackaChistova <159020125+TackaChistova@users.noreply.github.com>
2024-02-05 15:15:11 +0300

    popitka2

---
+++ easy.h
@@ -0,0 +1,7 @@
+#ifndef FILEHEADED_H_INCLUDED
+#define FILEHEADED_H_INCLUDED
+
+void itc_name();
+void itc_fio();
+
+#endif // FILEHEADED_H_INCLUDED
---
+++ taya.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "easy.h"
+
+using namespace std;
+
+void itc_name(){
+cout << "Taisiya";
+}
+
+void itc_fio(){
+itc_name();
+cout << " Chistova Alekseevna";
+
+}
